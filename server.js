e12a9eb02f991f9f04d21c609ed841062444b5e1
sparsh <sparshupadhyay@gmail.com>
2019-12-25 12:14:40 -0800

    changes commited

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 hbs.registerPartials(__dirname+"/views/partials");
 hbs.registerHelper('global',(name) => {
     console.log(name);
@@ -60,4 +62,4 @@ app.get('/home', (req, res) => {
 });
 
 
-app.listen(3000);
+app.listen(port);
